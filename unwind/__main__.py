44486e4e02b8cf436d6cc16849a96997499cdf33
William Bettridge-Radford <william.bettridge.radford@gail.com>
2023-01-06 22:44:48 +0000

    refactor main

--- unwind/__main__.py
+++ unwind/__main__.py
@@ -1,12 +1,32 @@
 import ast
+import argparse
 from pprint import pprint
 from sys import argv
 from pathlib import Path
+from rich.console import Console
 from . import unwind_file
 
-fn = Path(argv[1])
+console = Console(markup=False)
+python_print = print
+print = console.print
 
-tree = unwind_file(fn)
-for x in tree[1:]:
-    pprint(x)
+def dump_file(file):
+    with open(file) as f:
+        tree = ast.parse(f.read())
+        print(ast.dump(tree, indent=2))
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-dump", help="print ast", action="store_true")
+parser.add_argument("files", nargs='+', metavar='file')
+
+args = parser.parse_args()
+
+for file in args.files:
+    print(f'# {file}')
+    if args.dump:
+        dump_file(file)
+    else:
+        tree = unwind_file(file)
+        for x in tree[1:]:
+            pprint(x)
 
