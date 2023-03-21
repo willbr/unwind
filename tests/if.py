47e32c21d4fba7259a1d4314138d70ed0406e003
William Bettridge-Radford <william.bettridge.radford@gail.com>
2023-03-21 20:19:19 +0000

    test files

---
+++ tests/if.py
@@ -0,0 +1,10 @@
+if 1 > 0:
+    print(1)
+    print(2)
+elif 2 > 0:
+    print(3)
+    print(4)
+else:
+    print(5)
+    print(6)
+
---
+++ tests/math.py
@@ -0,0 +1,4 @@
+in_range = 1 < argc < 10
+a = 1 and 2
+b = 1 and 2 and 3
+c = 1 and 2 or 3
